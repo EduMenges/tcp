6a0af05950aa99b24124d385557a64db8bf53873
Eduardo Menges Mattje <eduardomattje@gmail.com>
2023-08-24 21:40:06 -0300

    Added function for midi boiler-plate

--- src/midi_action.rs
+++ src/midi_action.rs
@@ -20,6 +20,16 @@ impl MIDIaction {
     const INSTANT: u28 = u28::from_int_lossy(0);
     /// Pulses Per Quarter Note
     const DEFAULT_PPQN: u16 = 480;
+    const DEFAULT_TIME_SIGNATURE: midly::MetaMessage<'_> =
+        midly::MetaMessage::TimeSignature(4, 2, 24, 8);
+    const DEFAULT_KEY_SIGNATURE: midly::MetaMessage<'_> =
+        midly::MetaMessage::KeySignature(0, false);
+    const DEFAULT_MIDI_PORT: midly::MetaMessage<'_> = midly::MetaMessage::MidiPort(u7::from_int_lossy(0));
+    const TO_BE_ADDED: [MetaMessage<'_>; 3] = [
+        Self::DEFAULT_TIME_SIGNATURE,
+        Self::DEFAULT_KEY_SIGNATURE,
+        Self::DEFAULT_MIDI_PORT,
+    ];
 
     pub fn to_track<'a>(slice: &[Self]) -> Smf<'a> {
         let header: Header = Header {
@@ -30,12 +40,25 @@ impl MIDIaction {
 
         let mut track = Track::new();
 
+        // Add the default meta messages
+        Self::add_beggining(&mut track);
+
+        // Main loop
         for action in slice {
             action.push_as_event(&mut track)
         }
 
         smf.tracks.push(track);
         smf
+    }
+
+    fn add_beggining(track: &mut Track) {
+        for message in Self::TO_BE_ADDED {
+            track.push(TrackEvent {
+                delta: Self::INSTANT,
+                kind: TrackEventKind::Meta(message),
+            });
+        }
     }
 
     pub fn push_as_event(self, track: &mut Track) {
